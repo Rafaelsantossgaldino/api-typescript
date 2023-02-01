3bc46ff84cf2e8949328526bfeb8aebc45d948d1
rafael <rafaelsantossgaldino2@gmail.com>
2023-02-01 10:01:09 -0300

    config  the server

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,10 +1,9 @@
-import express from 'express';
+import express, { json } from 'express';
+import { router } from './routes';
 
 const server = express();
 
-server.get('/', (_, res) => {
-   return  res.send('Hello, world');
-});
-
+server.use(router);
+server.use(json());
 
 export { server };
