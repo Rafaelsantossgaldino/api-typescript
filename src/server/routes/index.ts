1bf0a2e26b132c0aeeca5803f4983a79f36371a1
rafael <rafaelsantossgaldino2@gmail.com>
2023-02-01 10:17:57 -0300

    add routes

---
+++ src/server/routes/index.ts
@@ -0,0 +1,17 @@
+import { Router } from 'express';
+import { StatusCodes }  from 'http-status-codes';
+
+const router = Router();
+
+router.get('/', (_, res) => {
+  return  res.send('Hello, world');
+});
+
+router.post('/teste', (req, res) => {
+
+  return res.status(StatusCodes.UNAUTHORIZED).json(req.body);
+});
+
+
+
+export { router };
