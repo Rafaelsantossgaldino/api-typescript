5f11bf4419157625ed38cc021b603142d9917f55
rafael <rafaelsantossgaldino2@gmail.com>
2023-02-02 12:59:51 -0300

    adicionando erros de validacao

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -3,13 +3,14 @@ import { StatusCodes } from 'http-status-codes';
 import * as yup from 'yup';
 
 
-
 interface ICidade {
   nome: string;
+  estado: string;
 }
 
 const bodyValidation: yup.SchemaOf<ICidade> = yup.object().shape({
   nome: yup.string().required().min(3),
+  estado: yup.string().required().min(3),
 });
 
 export const create = async (req:Request<{}, {}, ICidade>, res:Response) => {
@@ -17,14 +18,20 @@ export const create = async (req:Request<{}, {}, ICidade>, res:Response) => {
 
 
   try {
-    validatedData = await bodyValidation.validate(req.body);
+    validatedData = await bodyValidation.validate(req.body, { abortEarly: false });
   } catch (error){
     const yupError = error as yup.ValidationError;
+    const validationErrors:  Record<string, string> = {};
 
-    return res.json({
-      errors: {
-        default: yupError.message,
-      }
+    yupError.inner.forEach(error => {
+      if (error.path  === undefined) return;
+
+      validationErrors[error.path] = error.message;
+    });
+
+
+    return res.status(StatusCodes.BAD_REQUEST).json({
+      errors: validationErrors
     });
   }
 
