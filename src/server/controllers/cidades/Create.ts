5141f1726a460a3d6ee5731d6a53818f11be75d7
rafael <rafaelsantossgaldino2@gmail.com>
2023-02-03 10:48:12 -0300

    create validation

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -13,15 +13,15 @@ interface IFilter {
   filter?: string;
 }
 
-export const createValidation = validation({
-  body: yup.object().shape({
+export const createValidation = validation((getSchema) => ({
+  body: getSchema<ICidade>(yup.object().shape({
     nome: yup.string().required().min(3),
     estado: yup.string().required().min(3),
-  }),
-  query: yup.object().shape({
+  })),
+  query: getSchema<IFilter>(yup.object().shape({
     filter: yup.string().required().min(3),
-  }),
-});
+  })),
+}));
 
 export const create = async (req:Request<{}, {}, ICidade>, res:Response) => {
   console.log(req.body);
