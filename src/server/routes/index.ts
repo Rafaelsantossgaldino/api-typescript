76862a9ccb26cd19584bee2dbfcbb433d3305c2e
rafael <rafaelsantossgaldino2@gmail.com>
2023-02-15 11:54:49 -0300

    create controller getAll

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -9,11 +9,8 @@ router.get('/', (_, res) => {
   return  res.send('Hello, world');
 });
 
-router.post(
-  '/cidades',
-  CidadesController.createValidation,
-  CidadesController.create
-);
+router.get('/cidades', CidadesController.getAllValidation, CidadesController.getAll);
+router.post('/cidades', CidadesController.createValidation, CidadesController.create);
 
 
 export { router };
