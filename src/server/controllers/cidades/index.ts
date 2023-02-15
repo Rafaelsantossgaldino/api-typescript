c4be9c9029e1d2348021e65dbd5ca208d3d7c37d
rafael <rafaelsantossgaldino2@gmail.com>
2023-02-15 12:30:27 -0300

    import methods in controller

--- src/server/controllers/cidades/index.ts
+++ src/server/controllers/cidades/index.ts
@@ -1,8 +1,14 @@
 import * as  create  from './Create';
+import * as deleteByIdValidation from './DeleteById';
 import * as getAll from './GetAll';
+import * as  getById  from './GetById';
+import * as updateByIdValidation from './UpdateById';
 
 export const CidadesController =  {
+  ...updateByIdValidation,
+  ...deleteByIdValidation,
+  ...getById,
   ...create,
-  ...getAll
+  ...getAll,
 };
 
